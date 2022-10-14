67d94e0bea1cba3c315fe3c062f6096c02478ad6
junel <sacrojunel@gmail.com>
2022-10-14 11:06:04 +0800

    update: add global api route

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('v1/api');
+  app.enableCors();
   await app.listen(5003);
 }
 bootstrap();
