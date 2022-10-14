ccdc1493e1f9df8f0e78f3ad6b4e4a1c54e8d5e4
junel <sacrojunel@gmail.com>
2022-10-14 20:36:08 +0800

    update: add validation

--- src/calculator/dto/13monthpay.dto.ts
+++ src/calculator/dto/13monthpay.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty, IsNumber } from 'class-validator';
 export class IncentiveDto {
+  @IsNotEmpty()
+  @IsNumber()
   total_basic_pay: number;
+
+  @IsNotEmpty()
   name: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('v1/api');
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(5003);
 }
 bootstrap();
