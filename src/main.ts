0ca01c5d3974b74b005153d15d5f204552200fe9
junel <sacrojunel@gmail.com>
2022-12-04 20:55:42 +0800

    chore: add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('v1/api');
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.use(helmet());
   await app.listen(5003);
 }
 bootstrap();
