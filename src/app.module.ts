334da693ea660d5aa59560ba0c06a6abebea531b
junel <sacrojunel@gmail.com>
2022-12-09 23:26:18 +0800

    feat: add prisma in User.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Logger, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { PrismaService } from 'prisma.service';
 
 type User = any;
 
 @Injectable()
 export class UsersService {
+  constructor(private prisma: PrismaService) {}
   private readonly users = [
     {
       user_id: 1,
@@ -16,6 +19,11 @@ export class UsersService {
       password: 'junel',
     },
   ];
+  async user(userUniqueId: Prisma.UserWhereUniqueInput): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: userUniqueId,
+    });
+  }
 
   async findone(username: string): Promise<User | undefined> {
     return this.users.find((user) => user.username === username);
