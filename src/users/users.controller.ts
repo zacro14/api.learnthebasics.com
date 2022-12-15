0d3cc24cca64f996772babc19394905805bd2bab
junel <sacrojunel@gmail.com>
2022-12-15 16:52:22 +0800

    fix(userservicce): add status 404 the response if the user is not found

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,14 @@
 import {
-  BadRequestException,
   Body,
   Controller,
   Get,
   HttpException,
   HttpStatus,
-  Logger,
   Param,
   Patch,
-  Post,
   UseGuards,
 } from '@nestjs/common';
 import { AccessTokenGuard } from 'src/common/guards/accessToken.guard';
-import { CreatUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
 
@@ -23,36 +19,16 @@ export class UserController {
   @UseGuards(AccessTokenGuard)
   @Get(':id')
   async getUsers(@Param() params) {
-    try {
-      const user = this.user.getUser(params.id);
-      if (user) {
-        return user;
-      }
-      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-    } catch (error) {
-      Logger.error(error);
+    const user = await this.user.getUser(params.id);
+    console.log(user);
+    if (user) {
+      return user;
     }
+    throw new HttpException('User not found', HttpStatus.NOT_FOUND);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.user.update(id, updateUserDto);
   }
-
-  // @Post()
-  // async createUser(@Body() createUser: CreatUserDto) {
-  //   try {
-  //     const user = await this.user.createUser(createUser);
-  //     if (user) {
-  //       return {
-  //         success: true,
-  //         message: 'user created successfully',
-  //       };
-  //     } else {
-  //       throw new BadRequestException('Cannot create user');
-  //     }
-  //   } catch (error) {
-  //     Logger.error(error);
-  //   }
-  // }
 }
