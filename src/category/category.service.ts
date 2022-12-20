c07b824c5b1b88cf2a344125a0eaac27455ec06e
junel <sacrojunel@gmail.com>
2022-12-20 14:54:00 +0800

    feat: handle errors

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma.service';
-import { LessonCategory } from '@prisma/client';
+import { LessonCategory, Prisma } from '@prisma/client';
 import { CreateCategoryDto } from './dto/create-lesson-category.dto';
 
 @Injectable()
@@ -9,15 +9,13 @@ export class CategoryService {
 
   async createLessonCategory(
     createLessonCategory: CreateCategoryDto,
-  ): Promise<LessonCategory | null> {
+  ): Promise<LessonCategory> {
     try {
       const category = await this.prisma.lessonCategory.create({
         data: {
           ...createLessonCategory,
         },
       });
-
-      console.log('category', category);
 
       return category;
     } catch (error) {
