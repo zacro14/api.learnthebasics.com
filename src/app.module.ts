1d4632e60fc821fd50e6100bde8e7fc03b102f14
junel <sacrojunel@gmail.com>
2023-02-12 08:35:29 +0800

    feat: create the  resource for lesson end point

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
 import { UsersModule } from './users/users.module';
+import { LessonController } from './lesson/lesson.controller';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
@@ -10,6 +12,8 @@ import { UsersModule } from './users/users.module';
     AuthModule,
     UsersModule,
     CategoryModule,
+    LessonModule,
   ],
+  controllers: [LessonController],
 })
 export class AppModule {}
