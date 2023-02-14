738d1e05fb9546f1ffe998b072077947ffbee24e
junel <sacrojunel@gmail.com>
2023-02-14 15:52:28 +0800

    feat: create dtos

--- src/lesson/lesson.module.ts
+++ src/lesson/lesson.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'prisma.service';
 import { LessonController } from './lesson.controller';
 import { LessonService } from './lesson.service';
 
 @Module({
-  providers: [LessonService],
   controllers: [LessonController],
+  providers: [LessonService, PrismaService],
 })
 export class LessonModule {}
--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma.service';
 
 @Injectable()
-export class LessonService {}
+export class LessonService {
+  constructor(private prisma: PrismaService) {}
+
+  async createLesson() {
+    return 'lesson';
+  }
+}
