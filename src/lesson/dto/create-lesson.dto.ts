7d60535591da7304c545851b647e4926612e2718
junel <sacrojunel@gmail.com>
2023-02-14 20:49:33 +0800

    feat: create a model for creating lesson

--- src/lesson/dto/create-lesson.dto.ts
+++ src/lesson/dto/create-lesson.dto.ts
@@ -1,1 +1,14 @@
-export class createLessonDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class createLessonDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  subject: string;
+
+  @IsNotEmpty()
+  content: any;
+}
--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma.service';
+import { createLessonDto } from './dto/create-lesson.dto';
 
 @Injectable()
 export class LessonService {
   constructor(private prisma: PrismaService) {}
 
-  async createLesson() {
-    return 'lesson';
+  async createLesson(lessonData: createLessonDto): Promise<any> {
+    return 'any';
   }
 }
